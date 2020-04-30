f0812806ea5eb962adf66b7b13c63d59ee03dea7
HTsuruo <tsuru.dev@gmail.com>
2020-05-01 00:17:30 +0900

    chore: 微修正.

--- lib/pages/user.dart
+++ lib/pages/user.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:nippo/services/auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 @immutable
 class UserPage extends StatelessWidget {
@@ -12,22 +15,35 @@ class UserPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('登録者'),
       ),
-      body: ListView.builder(
-        itemBuilder: (_, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8),
-            child: ListTile(
-              title: Text(
-                'user $index .',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
-              ),
-              subtitle: const Text('Google認証'),
-              leading: Icon(Icons.people),
+      body: UserListView(listItems: listItems),
+    );
+  }
+}
+
+class UserListView extends StatelessWidget {
+  const UserListView({
+    @required this.listItems,
+  });
+
+  final List<String> listItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (_, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            title: Text(
+              'user $index .',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
             ),
-          );
-        },
-        itemCount: listItems.length,
-      ),
+            subtitle: const Text('Google認証'),
+            leading: Icon(Icons.people),
+          ),
+        );
+      },
+      itemCount: listItems.length,
     );
   }
 }
