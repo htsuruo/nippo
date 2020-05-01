aee62d187d9efe52890f7d0cd86efa71fcd08bd0
HTsuruo <tsuru.dev@gmail.com>
2020-05-01 15:49:09 +0900

    style: twitterぽい入力画面を追加.

--- lib/pages/create.dart
+++ lib/pages/create.dart
@@ -2,22 +2,79 @@ import 'package:flutter/material.dart';
 
 class CreatePage extends StatelessWidget {
   static const String routeName = '/create';
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('新しい日報を書く'),
-        backgroundColor: const Color(0xFFE84855),
-        iconTheme: IconThemeData(color: Colors.white),
-        textTheme: TextTheme(
-            title: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 16)),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            FlatButton(
+              child: const Text(
+                'キャンセル',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              onPressed: () => Navigator.pop(context),
+            ),
+            SubmitBtn(formKey: _formKey),
+          ],
+        ),
+        automaticallyImplyLeading: false,
+        titleSpacing: 16,
       ),
       body: SafeArea(
-        child: Container(),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Container(
+              child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'tenrefaf',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'please sssss';
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          )),
+        ),
+      ),
+    );
+  }
+}
+
+class SubmitBtn extends StatelessWidget {
+  const SubmitBtn({@required this.formKey});
+
+  final GlobalKey<FormState> formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      color: const Color(0xFFE84855),
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          print('success');
+          Navigator.pop(context);
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Text(
+          '投稿する',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
