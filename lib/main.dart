abfebe51613c2a1539226b1f7b2fc183e9207d67
htsuruo <tsuru.dev@gmail.com>
2023-06-20 00:27:49 +0900

    Add ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nippo/app.dart';
 import 'package:nippo/firebase_options.dart';
 
@@ -8,5 +9,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
 }
