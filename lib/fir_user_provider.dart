810b945a1404aa3280b63985e91a115a0899d69d
htsuruo <tsuru.dev@gmail.com>
2023-07-20 08:47:37 +0900

    Rename is_signed_in_provider to fir_user_provider

--- lib/fir_user_provider.dart
+++ lib/fir_user_provider.dart
@@ -1,16 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'is_signed_in_provider.g.dart';
+part 'fir_user_provider.g.dart';
 
 @riverpod
 Future<bool> isSignedIn(IsSignedInRef ref) {
-  return ref
-      .watch(authStateChangesProvider.future)
-      .then((user) => user != null);
+  return ref.watch(firUserProvider.future).then((user) => user != null);
 }
 
 @riverpod
-Stream<User?> authStateChanges(AuthStateChangesRef ref) {
+Stream<User?> firUser(FirUserRef ref) {
   return FirebaseAuth.instance.authStateChanges();
 }
--- lib/fir_user_provider.g.dart
+++ lib/fir_user_provider.g.dart
@@ -2,13 +2,13 @@
 
 // ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
 
-part of 'is_signed_in_provider.dart';
+part of 'fir_user_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$isSignedInHash() => r'9985992dc8f237de66c1441b94381831ad5b76e4';
+String _$isSignedInHash() => r'c27def8c3f17926899bb150f850224c6e81baee4';
 
 /// See also [isSignedIn].
 @ProviderFor(isSignedIn)
@@ -22,19 +22,18 @@ final isSignedInProvider = AutoDisposeFutureProvider<bool>.internal(
 );
 
 typedef IsSignedInRef = AutoDisposeFutureProviderRef<bool>;
-String _$authStateChangesHash() => r'06c05eec5a6c9127c308a9d2edb0019c311705c4';
+String _$firUserHash() => r'2c8d62d3e4771d17671a88a5b544ef02f4118a5c';
 
-/// See also [authStateChanges].
-@ProviderFor(authStateChanges)
-final authStateChangesProvider = AutoDisposeStreamProvider<User?>.internal(
-  authStateChanges,
-  name: r'authStateChangesProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$authStateChangesHash,
+/// See also [firUser].
+@ProviderFor(firUser)
+final firUserProvider = AutoDisposeStreamProvider<User?>.internal(
+  firUser,
+  name: r'firUserProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$firUserHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef AuthStateChangesRef = AutoDisposeStreamProviderRef<User?>;
+typedef FirUserRef = AutoDisposeStreamProviderRef<User?>;
 // ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
