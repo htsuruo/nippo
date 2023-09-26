97f5fca4de9a690640cf0c1f3bee843352dd2409
htsuruo <tsuru.dev@gmail.com>
2023-09-26 23:13:56 +0900

    cached_network_imageでシームレスに表示できるようにした

--- lib/common/widgets/circular_image.dart
+++ lib/common/widgets/circular_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CircularImage extends StatelessWidget {
@@ -7,22 +8,57 @@ class CircularImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     final imageUrl = this.imageUrl;
+    return imageUrl == null
+        ? const _Skelton()
+        : _NetworkImage(imageUrl: imageUrl);
+  }
+}
+
+const _imageSize = 100.0;
+
+class _Skelton extends StatelessWidget {
+  const _Skelton();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
+
     return Container(
-      width: 100,
-      height: 100,
+      width: _imageSize,
+      height: _imageSize,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
-        color: imageUrl == null ? colorScheme.onPrimary : null,
-        image: imageUrl == null
-            ? null
-            : DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(imageUrl),
-              ),
+        color: colorScheme.primary.withOpacity(.1),
       ),
+    );
+  }
+}
+
+class _NetworkImage extends StatelessWidget {
+  const _NetworkImage({required this.imageUrl});
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      placeholder: (context, url) => const _Skelton(),
+      imageBuilder: (context, imageProvider) {
+        return Container(
+          width: _imageSize,
+          height: _imageSize,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover,
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,6 +16,7 @@ import modal_progress_hud_nsn
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
+import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -30,5 +31,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
