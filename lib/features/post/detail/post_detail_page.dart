f02400f363279eee1f510ddfdc66b7416ac6ad07
htsuruo <tsuru.dev@gmail.com>
2023-10-12 16:07:20 +0900

    詳細画面のレイアウト調整

--- lib/features/post/detail/post_detail_page.dart
+++ lib/features/post/detail/post_detail_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nippo/common/common.dart';
 import 'package:nippo/features/post/post_provider.dart';
-import 'package:nippo/logger.dart';
 
 class PostDetailPage extends ConsumerWidget {
   const PostDetailPage({super.key, required this.uid, required this.pid});
@@ -11,13 +12,13 @@ class PostDetailPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+
     // TODO(htsuruo): familiyのうけとりをRecordsでTypeDef定義する
     final post = ref.watch(postProvider(uid, pid));
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('PostDetailPage'),
-      ),
+      appBar: AppBar(),
       body: post.when(
         data: (postSnapshot) {
           final post = postSnapshot.data();
@@ -26,15 +27,32 @@ class PostDetailPage extends ConsumerWidget {
               child: Text('データがありません'),
             );
           }
-          return Center(
-            child: Text(
-              post.title,
-              style: const TextStyle(fontSize: 24),
+          return SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  post.title,
+                  style: theme.textTheme.titleLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Gap(4),
+                Text('作成日: ${post.createdAt.date!.formatted}'),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Divider(),
+                ),
+                Text(
+                  post.description,
+                  style: theme.textTheme.bodyLarge,
+                ),
+              ],
             ),
           );
         },
         error: (error, stackTrace) {
-          logger.severe(error.toString());
           return Center(
             child: Text(error.toString()),
           );
