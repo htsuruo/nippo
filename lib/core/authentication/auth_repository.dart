8d4406f945e99b9a8adc96b07853e057885272d3
htsuruo <tsuru.dev@gmail.com>
2023-10-17 08:21:34 +0900

    wip: macOSでのGoogle認証対応
    - 併せて関連コードのリファクタリング

--- lib/core/authentication/auth_repository.dart
+++ lib/core/authentication/auth_repository.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:desktop_webview_auth/desktop_webview_auth.dart';
+import 'package:desktop_webview_auth/google.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -11,42 +15,67 @@ AuthRepository authRepository(AuthRepositoryRef ref) => AuthRepository();
 class AuthRepository {
   final _auth = FirebaseAuth.instance;
 
-  // v0.12からGIS(Google Identity Services)方式に変更となっている
-  // ref. https://pub.dev/packages/google_sign_in_web
-  Future<GoogleSignInAccount?> _signInWithWeb() async {
-    final googleSignIn = GoogleSignIn(
-      clientId:
-          // ignore: lines_longer_than_80_chars
-          '554602506203-i1vug0gcsln9gn83s29aatni3aftv6oj.apps.googleusercontent.com',
-    );
-    final res = await googleSignIn.requestScopes([
-      'email',
-      // Googleアカウント名やプロフィール画像の取得に必要
-      // ref. https://developers.google.com/identity/protocols/oauth2/scopes?hl=ja#people
-      'https://www.googleapis.com/auth/userinfo.profile',
-    ]);
-    return res ? googleSignIn.signInSilently() : null;
-  }
+  // `google_sign_in`プラグインパッケージでサポートされているプラットフォーム
+  final _isPackageSupportPlatform =
+      kIsWeb || Platform.isIOS || Platform.isAndroid;
+
+  static const _clientId =
+      // ignore: lines_longer_than_80_chars
+      '554602506203-i1vug0gcsln9gn83s29aatni3aftv6oj.apps.googleusercontent.com';
+  static const _scopes = [
+    'email',
+    // Googleアカウント名やプロフィール画像の取得に必要
+    // ref. https://developers.google.com/identity/protocols/oauth2/scopes?hl=ja#people
+    'https://www.googleapis.com/auth/userinfo.profile',
+  ];
+
+  Future<void> signOut() => _auth.signOut();
 
   Future<void> signInWithGoogle() async {
-    final googleAccount =
-        await (kIsWeb ? _signInWithWeb() : GoogleSignIn().signIn());
-    if (googleAccount == null) {
-      throw Exception('GoogleSignInAccount is null');
-    }
-    final googleAuth = await googleAccount.authentication;
-    if (googleAuth.idToken == null || googleAuth.accessToken == null) {
+    final (:idToken, :accessToken) = await (_isPackageSupportPlatform
+        ? _signInWithGoogleAccount()
+        : _signInWithDesktop());
+    if (idToken == null || accessToken == null) {
       throw Exception(
         'Google Signin Failed because idToken or accessToken is null',
       );
     }
     await _auth.signInWithCredential(
       GoogleAuthProvider.credential(
-        idToken: googleAuth.idToken,
-        accessToken: googleAuth.accessToken,
+        idToken: idToken,
+        accessToken: accessToken,
       ),
     );
   }
 
-  Future<void> signOut() => _auth.signOut();
+  // v0.12からGIS(Google Identity Services)方式に変更となっている
+  // ref. https://pub.dev/packages/google_sign_in_web
+  Future<GoogleSignInAccount?> _signInWithWeb() async {
+    final googleSignIn = GoogleSignIn(clientId: _clientId);
+    final res = await googleSignIn.requestScopes(_scopes);
+    return res ? googleSignIn.signInSilently() : null;
+  }
+
+  Future<({String? idToken, String? accessToken})>
+      _signInWithGoogleAccount() async {
+    final googleAccount =
+        await (kIsWeb ? _signInWithWeb() : GoogleSignIn().signIn());
+    if (googleAccount == null) {
+      throw Exception('GoogleSignInAccount is null');
+    }
+    final googleAuth = await googleAccount.authentication;
+    return (idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+  }
+
+  Future<({String? idToken, String? accessToken})> _signInWithDesktop() async {
+    final args = GoogleSignInArgs(
+      clientId: _clientId,
+      redirectUri: 'https://nippo-e8922.firebaseapp.com/__/auth/handler',
+      // TODO(htsuruo): スコープがListではなくStringで指定する形になっていて、
+      // 試しにカンマ区切りで投げてみたがうまく行かないので仕様を確認する
+      // scope: _scopes.join(','),
+    );
+    final res = await DesktopWebviewAuth.signIn(args);
+    return (idToken: res?.idToken, accessToken: res?.accessToken);
+  }
 }
