45a4220f6046121ba2e945132a059ba2f6da454f
htsuruo <tsuru.dev@gmail.com>
2023-11-03 22:53:02 +0900

    Revert "activate時の指定を修正"
    This reverts commit ec7299e27a0af40d71a7bb1b0b87460e6b15b225.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -26,14 +25,9 @@ Future<void> main() async {
     webProvider: ReCaptchaEnterpriseProvider(
       '6Le3MeQoAAAAANvhE-K5ZL2F7jwuE0GNQz1Pka_x',
     ),
-    // 一度デバッグトークンを登録しておけば、その後は優先的にデバッグトークンを使い続けるものと思っていたが、
-    // activate時に`debug`指定しないとサーバ検証時にエラーになってしまい
-    // トークンが返却されない（未検証リクエスト判定）ので、デバッグトークンを使いたい場合は`debug`指定が必要
-    androidProvider:
-        kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
-    appleProvider: kDebugMode
-        ? AppleProvider.debug
-        : AppleProvider.appAttestWithDeviceCheckFallback,
+    // デバッグトークンを新たに取得したい場合は以下コメントアウトを外して確認してください。
+    // androidProvider: AndroidProvider.debug,
+    // appleProvider: AppleProvider.debug,
   );
 
   // DateTimeのdefaultLocaleを日本時間にする
